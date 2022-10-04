b02fa40436ed5997b698e783d7238b2acf5ff056
Wagner Oliveira de Almeida <61123555+wagner477@users.noreply.github.com>
2022-10-03 21:07:02 -0300

    feat: Initial commit kkk

--- App.tsx
+++ App.tsx
@@ -1,5 +1,31 @@
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { Loading } from "@components/Loading/insex";
+
+import theme from "./src/theme";
 import { Groups } from "@screens/Groups";
 
 export default function App() {
-  return <Groups />;
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        animated
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
+  );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,8 +1,38 @@
-import { Container, Title } from "./styles";
+import { useState } from "react";
+import { FlatList } from "react-native";
+
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
+
+import { Container } from "./styles";
+import { ListEmpty } from "@components/ListEmpty";
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    "Galera da Rocketseat",
+    "Galera do Discord",
+    "Galera do Youtube",
+    "Galera do Twitch",
+    "Galera do Twitter",
+    "Galera do Facebook",
+    "Galera do Instagram",
+    "Galera do Linkedin",
+  ]);
+
   return (
     <Container>
-      <Title>Opa</Title>
+      <Header />
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 ? { flex: 1 } : {}}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Nenhuma turma encontrada" />
+        )}
+      />
     </Container>
   );
 }
